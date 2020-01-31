d8babf4b0ca31f71dce82d1f5abe1d045599df23
snail <710321444@qq.com>
2020-01-31 17:52:13 +0800

    123

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+error_reporting(E_ALL); // 项目部署后请将 E_All 修改为 0
+include 'phpGrace/phpGrace.php';
---
+++ phpGrace/config.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+	//数据库配置
+	'db'                 => array(
+		'databaseType'   =>    'mysql',
+    	'host'           =>    '127.0.0.1',
+    	'port'           =>    '3306',
+		'user'           =>    'root',
+		'pwd'            =>    'root',
+		'dbname'         =>    'test',
+		'charset'        =>    'utf8',
+		'pre'            =>    ''
+	),
+	//支持的缓存类型
+	'allowCacheType'     => array('file', 'memcache', 'redis'),
+	//缓存设置
+	'cache'             => array(
+		'type'          => 'file',
+		'pre'           => 'grace'
+	)
+);
